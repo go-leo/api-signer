49419b21ed7d9138a39d4c2611a734dfa44e398b
xiaoyan648 <1425895909@qq.com>
2023-03-10 14:10:33 +0800

    chore: 调整 example

--- example/example_test.go
+++ example/example_test.go
@@ -11,34 +11,6 @@ import (
 )
 
 func TestServer(t *testing.T) {
-
-	// 	url := "http://localhost:8080/admin/v1/assets/coins"
-	// 	method := "POST"
-
-	// 	payload := strings.NewReader(`{
-	//     "operation": 0,
-	//     "increase_req": {
-	//         "project": "newmedia_wechat",
-	//         "uid": 449724877852049850,
-	//         "amount": 100,
-	//         "way": 0,
-	//         "vaild_time_hour": 24
-	//     }
-	// }`)
-
-	// 	client := &http.Client{}
-	// 	req, err := http.NewRequest(method, url, payload)
-
-	// 	if err != nil {
-	// 		fmt.Println(err)
-	// 		return
-	// 	}
-	// 	req.Header.Add("Authorization", "APISIGNER-Standard-HMAC-SHA256 Credential=access key/20230309/cn-shanghai-1/asset/apisigner_request, Signature=c045475cff12fa08b1f51de9e2ac24007ffa339cd299ca154e73513295433de1")
-	// 	req.Header.Add("x-ca-date", "20230309T121235Z")
-	// 	req.Header.Add("x-ca-nonce", "b5185603-0177-485b-bba6-8b604d66276a")
-	// 	req.Header.Add("User-Agent", "apifox/1.0.0 (https://www.apifox.cn)")
-	// 	req.Header.Add("Content-Type", "application/json")
-
 	host := "http://127.0.0.1:8080"
 	path := "/admin/v1/assets/coins"
 	method := "POST"
@@ -52,16 +24,13 @@ func TestServer(t *testing.T) {
 			"vaild_time_hour": 24
 		}
 	}`)
-	// authorization := "APISIGNER-Standard-HMAC-SHA256 Credential=access key/20230306/region1/assets/apisigner_request, Signature=95e8a8f4cc4a2949a2bcb2e4041d2948f2b1655247ccce2f820d43b64805c056"
-
 	req, err := http.NewRequest(method, host+path, payload)
 	if err != nil {
 		panic(err)
 	}
 	req.Header.Add("Content-Type", "application/json")
 	Credential := apisigner.NewCredential("access key", "newmedia-sk")
-	Logger := apisigner.NewDefaultLogger(apisigner.LogInfo)
-	standard := apisigner.NewSigner(Logger)
+	standard := apisigner.NewSigner(apisigner.NewDefaultLogger(apisigner.LogInfo))
 	clientSDK := standard.Client("cn-shanghai-1", "asset", *Credential)
 	err = clientSDK.SetAuthHeader(req)
 	if err != nil {
@@ -83,3 +52,28 @@ func TestServer(t *testing.T) {
 	}
 	fmt.Println(string(body))
 }
+
+/*
+可以参考如下数据，判断客户端签名是否正确
+上面的 TestServer 方法请求签名产生的数据，如下:
+
+--- [Payload] --------------------------------
+POST
+/admin/v1/assets/coins
+
+faa665f426e861d659ccb5caf2b2ac52677c27bf2ce57a343f2384cc94ce6c73
+---[ STRING TO SIGN ]--------------------------------
+APISIGNER-HMAC-SHA256
+d0ddfe08-a0a1-4e30-b814-f9b16ad75ba1
+20230310T055928Z
+20230310/cn-shanghai-1/asset/apisigner_request
+46f58efa41cd58c5f37e67ad19bdb936ddc47f3f97ef51e66b6b3da39099e15f
+---[ Authorization ]--------------------------------
+APISIGNER-HMAC-SHA256 Credential=access key/20230310/cn-shanghai-1/asset/apisigner_request, Signature=438e6a6e81ee1a580ad8b80d4d788ff6e4da877714a8f35714a5ad96ef9912f4
+
+
+最终设置的请求头如下:
+APISIGNER-HMAC-SHA256 = APISIGNER-HMAC-SHA256 Credential=access key/20230310/cn-shanghai-1/asset/apisigner_request, Signature=438e6a6e81ee1a580ad8b80d4d788ff6e4da877714a8f35714a5ad96ef9912f4
+x-ca-nonce = d0ddfe08-a0a1-4e30-b814-f9b16ad75ba1
+x-ca-date = 20230310T055928Z
+*/
